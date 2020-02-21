fb35dd8de4b27d4a6f937dad97886ba8e7f61eb9
Vlad <yavlads2014@yandex.ru>
2020-02-21 19:34:56 +0300

    add auth for profile page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,7 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
-{
+{   
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     //
     /**
      * Show the application dashboard.
