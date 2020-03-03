d3f1596bdc2af2abdb40286131c178daec899a8a
Vlad <yavlads2014@yandex.ru>
2020-03-03 13:46:07 +0300

    фикс перехода в раздел с темой +  форматирование

--- resources/js/components/ThreadComponent.vue
+++ resources/js/components/ThreadComponent.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class="col-sm-12 col-md-6 col-lg-4 mt-2" v-on:click="toMessages"  >
-        <div class="card" style="width: 18rem;cursor: pointer;">  
-            <img class="card-img-top" :src="thread.image" alt="Card image cap" />
+    <div class="col-sm-12 col-md-6 col-lg-4 mt-2">
+        <div
+            class="card"
+            style="width: 18rem;cursor: pointer;"
+            v-on:click="toMessages"
+        >
+            <img
+                class="card-img-top"
+                :src="thread.image"
+                alt="Card image cap"
+            />
             <div class="card-body">
-                <p class="card-text">{{thread.title}}</p>
+                <p class="card-text">{{ thread.title }}</p>
             </div>
         </div>
     </div>
@@ -11,15 +19,11 @@
 
 <script>
 export default {
-    props:['thread'],
-    mounted() {
-       // console.log("Component mounted.");
-    },
-    methods:{
-        toMessages(){
-            window.location = `/thread/${this.thread.id}`
+    props: ["thread"],
+    methods: {
+        toMessages() {
+            window.location = `/thread/${this.thread.id}`;
         }
     }
 };
 </script>
-
