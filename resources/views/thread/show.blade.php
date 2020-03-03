3b6ef8552a6189cf677ef86e2643b117e172befc
Vlad <yavlads2014@yandex.ru>
2020-03-03 14:43:18 +0300

    Отображение  текста/картинки темы

--- resources/views/thread/show.blade.php
+++ resources/views/thread/show.blade.php
@@ -4,10 +4,17 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Тема</div>
+                <div class="card-header">
+                    Тема -
+                    <span class="font-weight-bold">{{$thread->title}}</span>
+                </div>
                 <div class="card-body">
-                    Тут диалог
-                    {{ $id }}
+                    <img
+                        src="{{$thread->image}}"
+                        class="rounded float-left"
+                        style="margin-right: 15px;"
+                    />
+                    <div class="text-justify">{{$thread->text}}</div>
                 </div>
             </div>
         </div>
